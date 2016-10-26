bac42c755b21f3dc05c3de0dafd046a30fe2ce44
Albert <awhite2017@chadwickschool.org>
2016-10-26 09:25:36 -0700

    Add multiplication

--- calculatorProject/calculatorProject.py
+++ calculatorProject/calculatorProject.py
@@ -6,15 +6,28 @@ def add(num1, num2): #addition
         result = result +1
     return result
     
-answer = add(12,12)
+answer = add(12,12) #answer = 24
 print(answer)
 
 def sub(num1, num2): #subtraction
-    result = numb1;
+    result = num1;
     for x in range(0,num2):
         print(x)
         result = result -1
     return result
   
-answer = sub(10,5)
+answer = sub(20,5) #answer = 15
 print(answer)
+
+def multiply(num1, num2): #multiplication
+    result = 0;
+    for x in range(num2):
+        print(x)
+        result = add(result, num1)
+    return result
+    
+answer = multiply(5,5) #answer = 25
+print(answer)
+  
+ 
+    
