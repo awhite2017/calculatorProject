8be597691a93553a0687f5a6e963a5ae52c5010a
Albert <awhite2017@chadwickschool.org>
2016-10-26 08:40:13 -0700

    addition and subtraction

--- calculatorProject/calculatorProject.py
+++ calculatorProject/calculatorProject.py
@@ -1,1 +1,20 @@
-d
+from Myro import *
+def add(num1, num2): #addition
+    result = num1;
+    for x in range(0,num2):
+        print(x)
+        result = result +1
+    return result
+    
+answer = add(12,12)
+print(answer)
+
+def sub(num1, num2): #subtraction
+    result = numb1;
+    for x in range(0,num2):
+        print(x)
+        result = result -1
+    return result
+  
+answer = sub(10,5)
+print(answer)
